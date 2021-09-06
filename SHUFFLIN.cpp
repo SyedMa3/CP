786b931cde4507d76fc3f9da089e4b0f2ad8d034
SyedMa3 <sammateen10@outlook.com>
2021-09-06 11:22:28 +0530

    CodeChef Problem - SHUFFLIN

---
+++ SHUFFLIN.cpp
@@ -0,0 +1,72 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
+    #define PI 3.1415926535897932384626
+    #define rep(n) for (int i = 0; i < n; ++i)
+    #define repc(i, n) for (ll i = 0; i < n; i++)
+    #define pb push_back
+    #define mp make_pair
+    #define F first
+    #define S second
+    #define all(x) x.begin(), x.end()
+    #define rall(x) x.rbegin(), x.rend()
+    #define sortall(x) sort(all(x))
+    #define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+    #define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it +)
+    typedef pair<int, int> pii;
+    typedef pair<ll, ll> pl;
+    typedef vector<int> vi;
+    typedef vector<ll> vl;
+    typedef vector<pii> vpii;
+    typedef vector<pl> vpl;
+    typedef vector<vi> vvi;
+    typedef vector<vl> vvl;
+
+    const unsigned int M = 1000000007;
+
+    int main()
+    {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+        int t = 1;
+        cin >> t;
+        while (t--)
+        {
+            int n;
+            cin >> n;
+            int sum = 0;
+            // vi a;
+            int o=1;
+            int e=2;
+            rep(n)
+            {
+                int x;
+                cin >> x;
+                // a.pb(x);
+
+                if(x%2 == 0)
+                {
+                    if(o<=n)
+                    {
+                        sum += 1;
+                        o += 2;
+                    }
+                }
+                else
+                {
+                    if(e<=n)
+                    {
+                        sum += 1;
+                        e += 2;
+                    }
+                }
+
+            }
+
+            cout << sum << "\n";
+
+
+        }
+
+        return 0;
+    }
