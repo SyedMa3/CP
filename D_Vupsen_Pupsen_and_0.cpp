af9290eaa78c89e86acae830a2fee7f954d4ac26
SyedMa3 <sammateen10@outlook.com>
2021-10-26 23:16:24 +0530

    CodeForces Problem - D_Vupsen_Pupsen_and_0

---
+++ D_Vupsen_Pupsen_and_0.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI 3.1415926535897932384626
+#define rep(n) for (int i = 0; i < n; ++i)
+#define repc(i, n) for (ll i = 0; i < n; i++)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sortall(x) sort(all(x))
+#define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it +)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+const unsigned int M = 1000000007;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vi a(n);
+        rep(n)
+        {
+            cin >> a[i];
+        }
+
+        if(n == 2)
+        {
+            cout << -a[1] << " " << a[0] << endl;
+            continue;
+        }
+        int i=0;
+
+        if(n%2 != 0)
+        {
+            // int x = std::lcm(a[1],a[2]);
+            // int l = std::lcm(a[0],x);
+            if(a[0] + a[1] != 0)
+            {
+                cout << a[2] << " " << a[2] << " " << -(a[0] + a[1]) << " ";
+            }
+            else if(a[1] + a[2] != 0)
+            {
+                cout << -(a[1] + a[2]) << " " << a[0] << " " << a[0] << " ";
+            }
+            else
+            {
+                cout << a[1] << " " << -(a[0] + a[2]) << " " << a[1] << " ";
+            }
+            i = 3;
+        }
+
+        for(; i<n-1; i+=2)
+        {
+            // int l = std::lcm(a[i],a[i+1]);
+            cout << -a[i+1] << " " << a[i] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
