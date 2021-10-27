255c5727d2efca49b625a7f7f4fea3b13d93eb2f
SyedMa3 <sammateen10@outlook.com>
2021-10-27 21:07:19 +0530

    CodeChef Problem - Alternating_Work_Days

---
+++ Alternating_Work_Days.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI 3.1415926535897932384626
+#define rep(n) for (int i = 0; i < n; ++i)
+#define repc(i, n) for (ll i = 0; i < n; i++)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sortall(x) sort(all(x))
+#define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it +)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+const unsigned int M = 1000000007;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int a,b,p,q;
+
+        cin >> a >> b >> p >> q;
+
+        if(p%a != 0 or q%b != 0)
+        {
+            cout << "NO\n";
+            continue;
+        }
+
+        int x = p/a;
+        int y = q/b;
+
+        if(abs(x-y) <= 1)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
