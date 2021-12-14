0617306402d980ef950322d079e443c5a93802d0
SyedMa3 <sammateen10@outlook.com>
2021-12-14 23:15:53 +0530

    CodeForces Problem - A_Polycarp_and_Sums_of_Subsequences

---
+++ A_Polycarp_and_Sums_of_Subsequences.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI 3.1415926535897932384626
+#define rep(n) for (int i = 0; i < n; ++i)
+#define repc(i, n) for (ll i = 0; i < n; i++)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sortall(x) sort(all(x))
+#define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it +)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+const unsigned int M = 1000000007;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int b[7];
+
+        rep(7)
+        {
+            cin >> b[i];
+        }
+
+        int a[3];
+        a[0] = b[0];
+        a[1] = b[1];
+
+        if(b[2] == a[0] + a[1])
+        {
+            a[2] = b[3];
+        }
+        else
+        {
+            a[2] = b[2];
+        }
+
+        rep(3)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
