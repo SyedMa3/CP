b8dc772e84832a249ab0cad04654c6d2e2c52642
SyedMa3 <sammateen10@outlook.com>
2022-01-09 16:09:36 +0530

    CodeForces Problem - C_Slay_the_Dragon

---
+++ C_Slay_the_Dragon.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI 3.1415926535897932384626
+#define rep(n) for (int i = 0; i < n; ++i)
+#define repc(i, n) for (ll i = 0; i < n; i++)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sortall(x) sort(all(x))
+#define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it +)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+const unsigned int M = 1000000007;
+
+ll solve(vl::iterator it, vl& a, ll sum,ll x, ll y, ll n)
+{
+    if(it != a.end())
+        {
+            ll c=0;
+            if(*it < x)
+                c=llabs(x-*it);
+
+            if(sum-*it >= y)
+            {
+                return c;
+            }
+            else
+            {
+                return llabs((sum-*it) - y)+c;
+            }
+        }
+        else
+        {
+            ll ans = llabs(x-a[n-1]);
+
+            if(sum-a[n-1] - y < 0)
+                ans += llabs(sum-a[n-1] - y);
+
+            return ans;
+        }
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    
+    int n;
+    cin >> n;
+
+    vl a(n);
+    ll sum = 0;
+
+    rep(n)
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
+    sortall(a);
+    
+    int m = 1;
+    cin >> m;
+    while (m--)
+    {
+        ll x,y;
+        cin >> x >> y;
+
+        auto it = lower_bound(all(a), x);
+        auto it2 = it-1;
+
+        ll ans = min(solve(it,a, sum,x,y,n), solve(it2,a,sum,x,y,n));
+
+        cout << ans << endl;
+
+    }
+
+    return 0;
+}
