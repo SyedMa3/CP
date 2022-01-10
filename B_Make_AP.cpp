98694822ec528fcd7aa40c812f2dd3fdd23d8865
SyedMa3 <sammateen10@outlook.com>
2022-01-10 22:57:44 +0530

    CodeForces Problem - B_Make_AP

---
+++ B_Make_AP.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI 3.1415926535897932384626
+#define rep(n) for (int i = 0; i < n; ++i)
+#define repc(i, n) for (ll i = 0; i < n; i++)
+#define fore(i,x1,x2) for(ll i=x1; i<x2; ++i)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sortall(x) sort(all(x))
+#define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it +)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+const unsigned int M = 1000000007;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int a,b,c;
+        cin >> a >> b >> c;
+
+        ll x1=2*b -c;
+        ll flag=0;
+        ll x2=-1;
+        if((a+c)%2==0)
+        {
+            x2 = (a+c)/2;
+            flag = 1;
+        }
+        ll x3 = 2*b - a;
+
+
+        if((x1%a == 0 and x1>0) or (flag==1 and x2%b==0) or (x3%c==0 and x3>0))
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
