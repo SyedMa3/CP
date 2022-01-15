89db184276b80f7eda437155844bc93149dc65a1
SyedMa3 <sammateen10@outlook.com>
2022-01-15 19:38:45 +0530

    CSES Problem - Palindrome_Reorder

---
+++ Palindrome_Reorder.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI 3.1415926535897932384626
+#define rep(n) for (int i = 0; i < n; ++i)
+#define repc(i, n) for (ll i = 0; i < n; i++)
+#define fore(i,x1,x2) for(ll i=x1; i<x2; ++i)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sortall(x) sort(all(x))
+#define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it +)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+const unsigned int M = 1000000007;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        vi a(26, 0);
+
+        int n = s.size();
+
+        rep(n)
+        {
+            a[s[i] - 'A']++;
+        }
+
+        int flag = 0;
+        int ind = -1;
+
+        rep(26)
+        {
+            if(a[i]%2)
+            {
+                flag++;
+                ind = i;
+            }
+        }
+
+        if(flag > 1)
+        {
+            cout << "NO SOLUTION\n";
+            continue;
+        }
+        string ans = "";
+
+        rep(26)
+        {              
+            repc(j, a[i]/2)
+            {
+                ans += (char)('A' + i);
+            }
+        }
+
+        
+        cout << ans;
+
+        if(ind != -1)
+        {
+            cout << (char)('A' + ind);
+        }
+
+        reverse(all(ans));
+        cout << ans << endl;
+
+
+
+    }
+
+    return 0;
+}
