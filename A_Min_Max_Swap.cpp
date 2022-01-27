876f46ad537fecfae44de0a51ddd86a3e319d195
SyedMa3 <sammateen10@outlook.com>
2022-01-28 00:20:18 +0530

    CodeForces Problem - A_Min_Max_Swap

---
+++ A_Min_Max_Swap.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI 3.1415926535897932384626
+#define rep(n) for (int i = 0; i < n; ++i)
+#define repc(i, n) for (ll i = 0; i < n; i++)
+#define fore(i,x1,x2) for(ll i=x1; i<x2; ++i)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sortall(x) sort(all(x))
+#define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it +)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+const unsigned int M = 1000000007;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vi a(n);
+        vi b(n);
+
+        int m1 = -1, m2 = -1;
+
+        rep(n)
+        {
+            cin >> a[i];
+        }
+        rep(n)
+        {
+            cin >> b[i];
+        }
+
+        rep(n)
+        {
+            if(a[i] < b[i])
+                swap(a[i], b[i]);
+        }
+
+        int x = *max_element(all(a));
+        int y = *max_element(all(b));
+
+        ll z = x*y;
+
+        cout << z << endl;
+
+
+    }
+
+    return 0;
+}
