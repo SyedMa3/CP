4719eb5c4d1293e4fd9a2d9445e1f609af38c4f1
SyedMa3 <sammateen10@outlook.com>
2022-02-23 17:21:34 +0530

    CodeForces Problem - A_Doors_and_Keys

---
+++ A_Doors_and_Keys.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI 3.1415926535897932384626
+#define rep(n) for (int i = 0; i < n; ++i)
+#define repc(i, n) for (ll i = 0; i < n; i++)
+#define fore(i,x1,x2) for(ll i=x1; i<x2; ++i)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sortall(x) sort(all(x))
+#define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it +)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+const unsigned int M = 1000000007;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        map <char,int> m;
+        string s;
+        cin >> s;
+
+        int flag = 0;
+        rep(6)
+        {
+            m[s[i]]++;
+            if(s[i] == 'R' or s[i] == 'B' or s[i] == 'G')
+            {
+                if(m[s[i]+32] == 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        if(flag)
+        {
+            cout << "NO\n";
+        }
+        else
+        {
+            cout << "YES\n";
+        }
+    }
+
+    return 0;
+}
