741484f66dcf2b621b24e7c0d419b666201c1637
SyedMa3 <sammateen10@outlook.com>
2022-03-27 19:45:58 +0530

    AtCoder Problem - A_-_Good_morning

---
+++ A_-_Good_morning.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI 3.1415926535897932384626
+#define rep(n) for (int i = 0; i < n; ++i)
+#define repc(i, n) for (ll i = 0; i < n; i++)
+#define fore(i,x1,x2) for(ll i=x1; i<x2; ++i)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sortall(x) sort(all(x))
+#define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it +)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+const unsigned int M = 1000000007;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        int a,b,c,d;
+        cin >> a >> b >> c >> d;
+
+        int x = a*60*60 + b*60;
+        int y = c*60*60 + d*60 + 1;
+
+        if(x<y)
+        {
+            cout << "Takahashi\n";
+        }
+        else
+        {
+            cout << "Aoki\n";
+        }
+    }
+
+    return 0;
+}
