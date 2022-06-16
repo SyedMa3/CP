abb5cf483765e501d17cebd781557eb25ba350e1
SyedMa3 <sammateen10@outlook.com>
2022-06-16 13:48:43 +0530

    CodeForces Problem - A_Parkway_Walk

---
+++ A_Parkway_Walk.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI 3.1415926535897932384626
+#define rep(n) for (int i = 0; i < n; ++i)
+#define repc(i, n) for (ll i = 0; i < n; i++)
+#define fore(i,x1,x2) for(ll i=x1; i<x2; ++i)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sortall(x) sort(all(x))
+#define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it +)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+const unsigned int M = 1000000007;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n,m;
+        cin >> n >> m;
+
+        vi a(n);
+        rep(n)
+        {
+            cin >> a[i];
+        }
+
+        ll ans = 0;
+
+        rep(n)
+        {
+            if(m - a[i] >= 0)
+            {
+                m -= a[i];
+                continue;
+            }
+
+            ans -= m;
+            m = 0;
+
+            ans += a[i];
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
